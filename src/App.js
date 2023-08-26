caf23ba8b3b7b8fa3948f699f98dd9b627f54548
JangHyunsu <wkd940709@naver.com>
2023-08-27 02:02:35 +0900

    갈아엎기 전

--- src/App.js
+++ src/App.js
@@ -8,25 +8,47 @@ function App() {
   return (
     
     <div className="App">
-      <h1 className="title">Todo-List</h1>
-      <div className="insertBox">
-        <div className="insertBoxTitle">
-          <input 
-            className="titleInput" 
-            type="text"
-            value={inputTitle}
-          ></input>
+      <h1 className="header">Todo-List</h1>
+      <div className="inputBox">
+        <div className="firstLine">
+          <div>
+            title
+          </div>
+          <div>
+            contents
+          </div>
         </div>
-        <div className="insertBoxContents">
-        <input 
-            className="contentsInput" 
-            type="text"
-            value={inputContents}
-          ></input>
+        <div className="secondLine">  
+          <div className="topTitle">
+            <input 
+              className="titleInput" 
+              type="text"
+              value={inputTitle}
+            ></input>
+          </div>
+          <div className="topContents">
+            <input 
+                className="contentsInput" 
+                type="text"
+                value={inputContents}
+              ></input>
+          </div>
+          <button className="addButton">add</button>
         </div>
 
        
 
+      </div>
+      <div className="bottom">
+        <div>box1</div>
+        <div>box2</div>
+        <div>box3</div>
+        <div>box4</div>
+        <div>box5</div>
+        <div>box6</div>
+        <div>box7</div>
+        <div>box8</div>
+        
       </div>
     </div>
   );
